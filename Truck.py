cd422224ba45122c7a4a0404c118d25499ef3d93
Tyler Gautney <tyler.gautney@codedragon.dev>
2023-05-11 23:19:19 -0500

    Eighth Commit

--- Truck.py
+++ Truck.py
@@ -16,21 +16,33 @@ class Truck:
 
     def determine_next_delivery(self, current_location: str) -> Package:
         min_distance: float = -1
-        next_delivery: Package = Package()
         for package in self.loaded_packages:
             distance: float = determine_distance(current_location, package.address)
             if min_distance < 0 or distance < min_distance:
                 min_distance = distance
-                next_delivery = package
+                next_delivery: Package = package
+        # noinspection PyUnboundLocalVariable
         return next_delivery
 
-    def load_packages(self, package_table: HashTable, current_location: str = '4001 South 700 East',
-                      keys: list[str] = None):
-        package: Package = Package()
+    def load_packages(self, package_table: HashTable):
 
-        def can_load() -> bool:
+        def get_related_packages(current_package: Package, unchecked_keys=None, all_keys=None) -> list[Package]:
+            if all_keys is None:
+                all_keys = list()
+            if unchecked_keys is None:
+                unchecked_keys = list()
+
+            all_keys.append(current_package)
+            unchecked_keys += list(map(int, re.findall(r'\d+', current_package.address)))
+            if len(unchecked_keys) == 0:
+                return all_keys
+            else:
+                next_package: Package = package_table.search(unchecked_keys.pop(0))
+                return get_related_packages(next_package, all_keys)
+
+        def can_load(package: Package) -> bool:
             # Return False unless the package passes all checks.
-            if package.notes == '' or notes_list[3].match(package.notes):
+            if package.notes == '':
                 return True
             elif notes_list[0].match(package.notes):
                 truck_number: int = int(re.search(r'\d+', package.notes).group())
@@ -39,36 +51,49 @@ class Truck:
             elif notes_list[1].match(package.notes) or notes_list[2].match(package.notes):
                 if self.route_number > 1:
                     return True
+            elif notes_list[3].match(package.notes):
+                packages = get_related_packages(package)
+                if len(self.loaded_packages) <= (16 - len(packages)):
+                    return True
             return False
 
-        def load_package():
-            if package is not None and can_load():
+        def load_package(package: Package, key: int):
+            if package is not None and can_load(package):
                 self.loaded_packages.append(package)
                 package.set_status()
                 package_table.remove(key)
 
-        if keys is None:
+        def load_nearest_packages(current_location: str):
             min_distance: float = -1
-            for key in range(1, package_table.size+1):
+            for key in range(1, package_table.size):
                 package: Package = package_table.search(key)
                 distance: float = determine_distance(current_location, package.address)
                 if (min_distance < 0 or distance < min_distance) and package.package_id is not None:
                     min_distance = distance
-                    load_package()
-        else:
-            for key in keys:
-                for loaded_package in self.loaded_packages:
-                    if not loaded_package.match_id(int(key)):
-                        package = package_table.search(int(key))
-                if package.package_id is not None:
-                    load_package()
+                    nearest_key: int = key
+                    nearest_package: Package = package
+            # noinspection PyUnboundLocalVariable
+            load_package(nearest_package, nearest_key)
+            return nearest_package
 
-        if len(self.loaded_packages) <= 16 and package.package_id is not None:
-            if notes_list[3].match(package.notes):
-                package_keys = re.search(r'[\d+, ]+', package.notes).group().split(', ')
-                self.load_packages(package_table=package_table, current_location=package.address, keys=package_keys)
-            else:
-                self.load_packages(package_table=package_table, current_location=package.address)
+        def load_related_packages(package: Package):
+            packages = get_related_packages(package)
+            for package in packages:
+                load_package(package, package.package_id)
+            return package
+
+        def __init__(package: Package = None, current_location: str = '4001 South 700 East'):
+            if len(self.loaded_packages) == 0:
+                package = load_nearest_packages(current_location)
+            elif 0 < len(self.loaded_packages) < 16 and package is not None:
+                if notes_list[3].match(package.notes):
+                    package = load_related_packages(package)
+                else:
+                    package = load_nearest_packages(current_location)
+            if package is not None:
+                __init__(package=package, current_location=package.address)
+
+        __init__()
 
     def deliver_packages(self, time_elapsed: datetime, current_location: str = '4001 South 700 East',
                          total_distance: float = 0):
